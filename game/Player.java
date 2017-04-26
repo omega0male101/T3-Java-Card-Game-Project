af8fa96907549383745b7fcf69840692b9459d28
Eden <edenstevens101@gmail.com>
2017-04-26 12:52:50 +0100

    Started the Game Class, needs to be tested.

---
+++ game/Player.java
@@ -0,0 +1,28 @@
+public class Game {
+    Dealer dealer;
+    ArrayList<Player> players;
+
+
+    public Game(Dealer dealer) {
+        this.dealer = dealer;
+        this.players = new ArrayList<player>;
+    }
+
+    public String checkWinner(){
+        int player1Points = players.get(0).totalPoints();
+        int player2Points = players.get(1).totalPoints();
+
+            if (player1Points > player2Points){
+                return "PLayer One Wins!"
+            }
+            else if (player2Points > player1Points){
+                return "Player Two Wins!"
+            }
+            else if (player2Points == player1Points){
+                return "What are the chances? It's a draw!"
+            }
+            else {
+                return "Error 1 - Something isn't right / Check PLayer Class"
+            }
+    }
+}
